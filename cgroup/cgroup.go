5f23d5bfc7f3d745e0d3dfba57571ad91ea4a8e7
howu911 <496272917@qq.com>
2022-04-09 18:52:15 +0800

    add cgroup

---
+++ cgroup/cgroup.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+// 挂载memory subsystem的hierarchy的根目录位置
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+func main() {
+	if os.Args[0] == "/proc/self/exe" {
+		// 容器进程
+		fmt.Printf("cur pid is %d", syscall.Getpid())
+		fmt.Println()
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil { //go封装了对于系统clone()函数的调用，这段代码执行后会进入一个sh运行环境中
+			log.Fatal(err)
+		}
+	}
+
+	cmd := exec.Command("/proc/self/exe") // 指定被fork出来的新进程内的初始命令
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC, //使用CLONE_NEWUTS标识来创建一个UTC namesapce
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("%v", cmd.Process.Pid)
+
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("100m"), 0644)
+	}
+	cmd.Process.Wait()
+}
